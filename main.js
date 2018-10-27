a8957cf1d8d329482b9896696871aabeb2cb4bc8
nightyknite <nightyknite@gmail.com>
2018-10-27 12:16:06 +0900

    add atnd

--- main.js
+++ main.js
@@ -12,28 +12,65 @@
         week : 'week',
         day  : 'day'
       },
-      minTime:'00:00:00',
-      maxTime:'24:00:00',      
+      axisFormat: 'H(:mm)',
+      timeFormat: 'H(:mm)',
       events    : function(start, end, timezone, callback) {
         var events = [];
         var ym = start.add(7, 'days').format("YYYYMM"); 
-        $.ajax({
+        $.when(
+          $.ajax({
             url: "https://connpass.com/api/v1/event/?count=100&ym="+ym+"",
             dataType: 'jsonp',
-            success: function(data){
-              for (var i in data.events) {
-                events.push({
-                  title: data.events[i].title,
-                  start: moment(data.events[i].started_at),
-                  end: moment(data.events[i].ended_at),
-                  url: data.events[i].event_url,
-                  backgroundColor: '#a82400',
-                  borderColor: '#a82400'
-                });
-              }
-              callback(events);
+          }),
+          $.ajax({
+            url: "http://api.atnd.org/events/?count=100&ym="+ym+"&start=1&format=jsonp",
+            dataType: 'jsonp',
+          })
+        )
+        .done(function(data1, data2) {
+            console.log(data1);
+            for (var i in data1[0].events) {
+              events.push({
+                title: data1[0].events[i].title,
+                start: moment(data1[0].events[i].started_at),
+                end: moment(data1[0].events[i].ended_at),
+                url: data1[0].events[i].event_url,
+                backgroundColor: '#a82400',
+                borderColor: '#a82400'
+              });
+            
             }
+
+
+            for (var i in data2[0].events) {
+              events.push({
+                title: data2[0].events[i].event.title,
+                start: moment(data2[0].events[i].event.started_at),
+                end: moment(data2[0].events[i].event.ended_at),
+                url: data2[0].events[i].event.event_url,
+                backgroundColor: '#EBAC2B',
+                borderColor: '#EBAC2B'
+              });
+              
+            }
+
+            callback(events);
+
+          
+        })
+        .fail(function() {
+            console.log('error');
         });
+      
+
+        //console.log(events);
+
+        //callback(events);
+
+
+
+        
+
       },
       editable  : true,
       eventLimit: false,
